c4d543bf1accda32eb4076cb77fee9ff40792fc9
fayazmohd123 <fayazmohd323@gmail.com>
2026-01-05 06:41:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I am alive"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
